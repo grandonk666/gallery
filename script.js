4a43c7217d413fabcea6c714efa0931119457515
grandonk666 <grandonk457@gmail.com>
2020-10-30 17:51:49 +0700

    fixing minor problem

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
-const menuToggle = document.querySelector(".toggle input");
-const nav = document.querySelector("nav ul");
-menuToggle.addEventListener("click", function () {
-  nav.classList.toggle("slide");
+// const menuToggle = document.querySelector(".toggle input");
+// const nav = document.querySelector("nav ul");
+// menuToggle.addEventListener("click", function () {
+//   nav.classList.toggle("slide");
+// });
+
+
+
+// const serviceIcon = document.querySelectorAll(".service");
+// serviceIcon.forEach(function(el){
+//   el.addEventListener('mouseenter',function(e){
+//     e.target.lastElementChild.style.display = 'block';
+//   });
+//   el.addEventListener('mouseleave',function(e){
+//     e.target.lastElementChild.style.display = 'none';
+//   });
+// });
+
+$(document).ready(function(){
+  $(".toggle input").click(
+    function(){
+      $("nav ul").toggleClass("slide")
+    }
+  );
+
+
+  $('.service').hover(
+    function(){
+      $(this.lastElementChild).slideDown(500);
+    },function(){
+      $(this.lastElementChild).slideUp(500);
+    }
+  );
 });
-
-
-const serviceIcon = document.querySelectorAll(".icon");
-serviceIcon.forEach(function(el){
-  el.parentNode.addEventListener('mouseenter',function(e){
-    e.target.lastElementChild.style.display = 'block';
-  });
-  el.parentNode.addEventListener('mouseleave',function(e){
-    e.target.lastElementChild.style.display = 'none';
-  });
-});
-
